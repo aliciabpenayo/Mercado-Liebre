ea5e14865ead05474a082af17bbbf2db7f701204
aliciabpenayo <aliciabpenayo@gmail.com>
2022-06-02 17:18:49 -0300

    actualizacion para Heroku

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port= process.env.PORT ||3000;
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000')
+app.listen(port, () => {
+    console.log(`Servidor corriendo en el puerto ${port}`)
 });
 
 app.get('/', (req, res) => {
